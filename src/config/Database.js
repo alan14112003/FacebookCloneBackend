4182db4f88cf69b57d02be27ac2338d11d6b24f8
alan14112003 <sonnn.21it@gmil.com>
2023-03-30 15:19:17 +0700

    feat: ConfigDatabaseMongoDB

---
+++ src/config/Database.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const connections = {
+  async MongoDB() {
+    try {
+      const connect = await mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      console.log(`connected ${connect.connection.host}`)
+    } catch (e) {
+      console.error(`Error: ${error.message}`)
+      process.exit()
+    }
+  },
+}
+
+export default connections
--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
 
+import connections from './config/database'
+
 // Cấu hình dotenv
 dotenv.config()
+
+// kết nối tới DB của mongo
+connections.MongoDB()
 
 // tạo app là đường dẫn gốc trong thư mục
 const app = express()
@@ -12,6 +17,6 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
 // gắn nghe cho app và gán vào server
-const server = app.listen(process.env.PORT || 443, () => {
+const server = app.listen(process.env.PORT || 80, () => {
   console.log(`Server đang chạy ở cổng ${process.env.PORT}`)
 })
